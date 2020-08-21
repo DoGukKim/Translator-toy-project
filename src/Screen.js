ae91a202fb72a27bf2ef4115962000619f88971a
DOGUK <dgkim3811@naver.com>
2020-08-21 19:01:41 +0900

    UpdateLangAndReadme

--- src/Screen.js
+++ src/Screen.js
@@ -6,7 +6,7 @@ export default () => {
   const t = useT();
   return (
     <>
-      <h1>{t("Hello!")}</h1>
+      <h1>{t("Hello")}</h1>
       <button onClick={() => setLang("kr")}>{t("Translate")}</button>
     </>
   );
--- src/translations.js
+++ src/translations.js
@@ -1,6 +1,6 @@
 const translations = {
   kr: {
-    "Hello!": "안녕하세요!",
+    Hello: "안녕하세요",
     Translate: "번역하다",
   },
 };
